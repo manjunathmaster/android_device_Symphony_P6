85ed420f6b927348052108e3260e388e3bfc8a9e
a7raj <nxaahraj@gmail.com>
2018-06-07 04:01:55 -0700

    Bringup N

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -10,4 +10,4 @@ $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr:
 endif
 
 # include the non-open-source counterpart to this file
--include vendor/Micromax/AQ5001/AndroidBoardVendor.mk
+-include vendor/Symphony/P6/AndroidBoardVendor.mk
